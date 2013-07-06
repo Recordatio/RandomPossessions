aab45c297309c0b7cc6ce61b7c9f65e9c7fa1516
Alexis <alexiswu@Alexiss-MacBook-Pro.local>
2013-07-06 21:02:33 +0200

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Alexis on 2013-07-06.
+//  Copyright (c) 2013 Brawu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
